8a2af6149532c677843839fde4d40d4d5e69d2aa
robsonmars <robsonmarinho1@gmail.com>
2026-06-07 14:53:09 -0300

    Rviz.png Comandos.txt gazebo.launch > virar robô e iniciar em pé

--- ros2_ws/src/otto_description/launch/gazebo.launch.py
+++ ros2_ws/src/otto_description/launch/gazebo.launch.py
@@ -78,7 +78,8 @@ def generate_launch_description():
         arguments=[
             '-topic', 'robot_description',
             '-name', 'otto',
-            '-z', '0.3',
+            '-z', '0.15',
+            '-Y', '1.5707963',  # rotação 90° em yaw se necessário
         ],
         output='both'
     )
